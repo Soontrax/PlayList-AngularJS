8ac327a6a6ebcb46dc5d5124b404bad5cb2d68b5
Soontrax <32308953+Soontrax@users.noreply.github.com>
2019-03-07 22:56:28 +0100

    Add files via upload

--- js/tareas.js
+++ js/tareas.js
@@ -3,25 +3,134 @@ angular.module('todoAp', [])
 
         var ctrl = this;
 
-        ctrl.musicas = [
-            {
+        ctrl.musicas = [{
                 ID: "1",
                 Nombre: "Bill Eat",
                 Autor: "M. Jackson",
                 Genero: "Pop",
-                Duracion: "5:20"
+                Duracion: "5:20",
+                num_canciones: "1,2,3"
             },
             {
                 ID: "2",
                 Nombre: "Mia",
                 Autor: "Bad Bunny",
                 Genero: "Reguetón",
-                Duracion: "3:30"
+                Duracion: "3:30",
+                num_canciones: "4,5,6"
             },
+            {
+                ID: "3",
+                Nombre: "Pa' Llamar tu atención",
+                Autor: "C. Tangana",
+                Genero: "Reguetón",
+                Duracion: "2:58",
+                num_canciones: "1,2,3"
+            },
+            {
+                ID: "4",
+                Nombre: "Bangarang",
+                Autor: "Skrillex",
+                Genero: "Electrónica",
+                Duracion: "3:39",
+                num_canciones: "1,2,3"
+            },
+            {
+                ID: "5",
+                Nombre: "A prueba de ti",
+                Autor: "Malú",
+                Genero: "Pop",
+                Duracion: "3:29",
+                num_canciones: "1,2,3"
+            },
+            {
+                ID: "6",
+                Nombre: "El circulo",
+                Autor: "KSO",
+                Genero: "Rap",
+                Duracion: "5:29",
+                num_canciones: "1,2,3"
+            },
+            {
+                ID: "7",
+                Nombre: "Deseo más y mas",
+                Autor: "La Union",
+                Genero: "Pop",
+                Duracion: "3:32",
+                num_canciones: "1,2,3"
+            }
+
         ];
 
+        ctrl.canciones = [{
+                ID: "1",
+                Nombre: "Bill Eat",
+                Autor: "M. Jackson",
+                Genero: "Pop",
+                Duracion: "5:20",
+                num_canciones: "1,2,3"
+            },
+            {
+                ID: "2",
+                Nombre: "Mia",
+                Autor: "Bad Bunny",
+                Genero: "Reguetón",
+                Duracion: "3:30",
+                num_canciones: "1,2,3"
+            },
+            {
+                ID: "3",
+                Nombre: "Pa' Llamar tu atención",
+                Autor: "C. Tangana",
+                Genero: "Reguetón",
+                Duracion: "2:58",
+                num_canciones: "1,2,3"
+            },
+            {
+                ID: "4",
+                Nombre: "Bangarang",
+                Autor: "Skrillex",
+                Genero: "Electrónica",
+                Duracion: "3:39",
+                num_canciones: "1,2,3"
+            },
+            {
+                ID: "5",
+                Nombre: "A prueba de ti",
+                Autor: "Malú",
+                Genero: "Pop",
+                Duracion: "3:29",
+                num_canciones: "1,2,3"
+            },
+            {
+                ID: "6",
+                Nombre: "El circulo",
+                Autor: "KSO",
+                Genero: "Rap",
+                Duracion: "5:29",
+                num_canciones: "1,2,3"
+            },
+            {
+                ID: "7",
+                Nombre: "Deseo más y mas",
+                Autor: "La Union",
+                Genero: "Pop",
+                Duracion: "3:32",
+                num_canciones: "1,2,3"
+            }
+
+        ];
+
+        $scope.order = function (x) {
+            $scope.order = x;
+        }
+
         ctrl.agregarMusica = function () {
-            ctrl.musicas.push({ Nombre: ctrl.nombre, Genero: ctrl.genero, hecho: false });
+            ctrl.musicas.push({
+                Nombre: ctrl.nombre,
+                Genero: ctrl.genero,
+                hecho: false
+            });
             ctrl.textoNuevaTarea = '';
         };
 
@@ -34,7 +143,7 @@ angular.module('todoAp', [])
         };
 
         ctrl.mostrarCanciones = function (index) {
-            alert("mostrar canciones" + index);
+            alert("mostrar canciones del indice: " + index);
         };
 
         ctrl.eliminar = function () {
